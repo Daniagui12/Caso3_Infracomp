a7803698685a8b2e747e3a00f3af77400b07564a
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-11-03 15:01:15 -0500

    update clientesecurity

--- Caso 3/src/seguridad20222_cliente/ClienteMain.java
+++ Caso 3/src/seguridad20222_cliente/ClienteMain.java
@@ -2,4 +2,8 @@ package seguridad20222_cliente;
 
 public class ClienteMain {
 
+    private static final String ID = "Main Client: ";
+    private static int puerto = 4030;
+    private static String host = "localhost";
+    
 }
--- Caso 3/src/seguridad20222_cliente/ClienteThread.java
+++ Caso 3/src/seguridad20222_cliente/ClienteThread.java
@@ -1,5 +1,26 @@
 package seguridad20222_cliente;
 
-public class ClienteThread {
+import java.math.BigInteger;
+import java.net.Socket;
+
+public class ClienteThread extends Thread {
+
+    private Socket socket;
+    private String id;
+	private String dlg;	
+	private BigInteger p;
+	private BigInteger g;
+	private ClienteSecurityFunctions f;	
+	private int mod;
+
+    ClienteThread(Socket socket, String id, String dlg, BigInteger p, BigInteger g, int mod) {
+        this.socket = socket;
+        this.id = id;
+        this.dlg = dlg;
+        this.p = p;
+        this.g = g;
+        this.mod = mod;
+        f = new ClienteSecurityFunctions();
+    }
 
 }
