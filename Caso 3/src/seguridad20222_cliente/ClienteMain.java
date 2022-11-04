4569d283270bb32914ee030dfeb92a4d96ef17c3
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-11-03 19:20:25 -0500

    conexion cliente servidor realizada

--- Caso 3/src/seguridad20222_cliente/ClienteMain.java
+++ Caso 3/src/seguridad20222_cliente/ClienteMain.java
@@ -1,9 +1,26 @@
 package seguridad20222_cliente;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClienteMain {
 
-    private static final String ID = "Main Client: ";
-    private static int puerto = 4030;
-    private static String host = "localhost";
+    private static Socket sc;
+	private static String host = "localhost";
+	private static int puerto = 4030;
+    public static void main(String[] args) {
+        
+        System.out.println("Starting client");
+        try {
+
+            sc = new Socket(host, puerto);
+            System.out.println("Creating socket: done");
+            ClienteThread ct = new ClienteThread(sc, "1");
+            ct.start();
+
+        } catch (Exception e) { e.printStackTrace(); }
+    }
     
 }
--- Caso 3/src/seguridad20222_cliente/ClienteThread.java
+++ Caso 3/src/seguridad20222_cliente/ClienteThread.java
@@ -1,26 +1,44 @@
 package seguridad20222_cliente;
 
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.PublicKey;
 
 public class ClienteThread extends Thread {
 
-    private Socket socket;
+    private Socket sc;
     private String id;
-	private String dlg;	
-	private BigInteger p;
-	private BigInteger g;
-	private ClienteSecurityFunctions f;	
-	private int mod;
+    private PublicKey publicKey;
+    private ClienteSecurityFunctions csf;
 
-    ClienteThread(Socket socket, String id, String dlg, BigInteger p, BigInteger g, int mod) {
-        this.socket = socket;
+    ClienteThread(Socket socket, String id) {
+        this.sc = socket;
         this.id = id;
-        this.dlg = dlg;
-        this.p = p;
-        this.g = g;
-        this.mod = mod;
-        f = new ClienteSecurityFunctions();
+        this.csf = new ClienteSecurityFunctions();
     }
 
+    @Override
+	public void run() {
+		
+		String linea;
+	
+
+	    try {
+
+			PublicKey publicKey = csf.read_kplus("Caso 3/datos_asim_srv.pub", this.id);
+			PrintWriter ac = new PrintWriter(sc.getOutputStream() , true);
+			BufferedReader dc = new BufferedReader(new InputStreamReader(sc.getInputStream()));
+				    	
+            ac.println("SECURE INIT");
+            linea = dc.readLine();
+            System.out.println("Cliente " + id + ": recibido " + linea + " - done");
+    		
+
+	        sc.close();
+	    } catch (Exception e) { e.printStackTrace(); }
+
+	}
+        
 }
